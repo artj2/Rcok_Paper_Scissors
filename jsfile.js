ed3edbd94106cf479e60673860f2ca2b4b16ae1b
ArtJ <artiziom12@gmail.com>
2022-01-16 21:24:26 +0000

    Created function playRound()

--- jsfile.js
+++ jsfile.js
@@ -1,6 +1,29 @@
 function computerPlay() {
-    const options = ["Rock", "Paper", "Scissors"];
+    const options = ["rock", "paper", "scissors"];
     let random = Math.floor(Math.random() * options.length);
     return (options[random]);
-    
+}
+
+function playRound(playerSelection) {
+    let playerSelection = playerSelection.toLowerCase();
+    let computerSelection = computerPlay();
+    if (playerSelection == computerSelection) {
+        return "Draw";
+    }
+    if (playerSelection == "rock") {
+        if (computerSelection == "scissors"){
+            return "You Win! Rock beats Scissors";
+        }
+        else {
+            return "You Lose! Paper beats Rock";
+        }
+    }
+    if (computerSelection == "rock") {
+        if (playerSelection == "scissors") {
+            return "You Lose! Rock beats Scissors";
+        }
+        else {
+            return "You Win! Paper beats Rock";
+        }
+    }
 }
